7f3ebec6d3d5ae2fe8677b44924e7c7db0e966e7
Vinicius Ferrarini <vinicius.ferrarini@softfocus.com.br>
2020-03-10 23:54:17 -0300

    First Commit

---
+++ src/test/java/br/com/softfocus/gamesapi/GamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softfocus.gamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
